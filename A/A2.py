7a794f81730e29aa109f6472f4b41fbf62f659e1
aldo <aldo.matus@outlook.com>
2022-02-05 13:50:56 -0600

    A2 activity

--- A/A2.py
+++ A/A2.py
@@ -1,12 +1,75 @@
-from data import dictionary, dictionary_of_levels
+from data import dictionary, dictionary_of_levels, dictionary_of_levels2
+import ast
 
 def show(dictionary):
+    sort_names = {}
+    names_first_layer = []
+    names_second_layer = []
+    names_third_layer = []
+    
     for key in dictionary.keys():
-        dictionary[key]['Level'] = dictionary_of_levels[dictionary[key]['Level']]
-        dictionary[key]['name']
+        for number_layer_1 in range(len(dictionary)):
+            if number_layer_1+1 == dictionary_of_levels.get(dictionary[key]['Level'], 0) and type(dictionary[key]['Level']) != int:
+                dictionary[key]['Level'] = number_layer_1+1
+                names_first_layer.append(dictionary[key]['name'])
+                sort_names[1] = names_first_layer
 
-    print(dictionary)
-            
 
+        subdata_number = sum(1 if type(x) == dict else 0 for x in dictionary[key].values())
+
+        for key2 in dictionary[key].keys():
+            if type(dictionary[key][key2]) == dict:
+                #print('name: ', dictionary[key][key2]['name'])
+                for number_layer_2 in range(subdata_number):
+                    if number_layer_2+1 == dictionary_of_levels.get(dictionary[key][key2]['Level'], 0) and type(dictionary[key][key2]['Level']) != int:
+                        dictionary[key][key2]['Level'] = number_layer_2+1
+                        names_second_layer.append(dictionary[key][key2]['name'])
+                        sort_names[2] = names_second_layer
+
+        subdata_number = sum(1 if type(x) == dict else 0 for x in dictionary[key].values())
+
+        for key3 in dictionary[key][key2].keys():
+            if type(dictionary[key][key2][key3]) == dict:
+                #print('name: ', dictionary[key][key2][key3]['name'])
+                for number_layer_3 in range(subdata_number):
+                    if number_layer_3+1 == dictionary_of_levels.get(dictionary[key][key2][key3]['Level'], 0) and type(dictionary[key][key2][key3]['Level']) != int:
+                        dictionary[key][key2][key3]['Level'] = number_layer_3+1
+                        names_third_layer.append(dictionary[key][key2][key3]['name'])
+                        sort_names[3] = names_third_layer
+    f = open("/home/aldo/Documents/envio_click/A/A2_1.txt","w+")
+    f2 = open("/home/aldo/Documents/envio_click/A/A2_2.txt","w+")
+    f.write(str(sort_names))
+    f2.write(str(dictionary))
+    f.close()
+    f2.close()
+
+    counter = 1
+    for sort_name in sort_names.values():
+        for name in sort_name:
+            print("\t"*counter + str(name))
+        print('\n')
+        counter+=2
+
+
+def addData(levels, name):
+    f = open("A2_1.txt","w+")
+    with open('/home/aldo/Documents/envio_click/A/A2_2.txt', 'r+') as dictionary:
+        contents = dictionary.read()
+        dictionary = ast.literal_eval(contents)
+
+    # for level in levels:
+    #     for 
+
+
+    
 if __name__ == '__main__':
     show(dictionary)
+    # levels = []
+    # last_level = int(input("Enter your last level: "))
+    # for level_number in range(last_level):
+    #     level_num = int(input(f"Enter your category of level{level_number}: (example: enter 1 to select DataA or enter 2 to select DataB): "))
+    #     levels.append(level_num)
+
+    
+    # name = str(input("Enter your name: "))
+    # addData(levels, name)
--- A/data.py
+++ A/data.py
@@ -1,13 +1,15 @@
 dictionary = {
     "DataA": {
-                "name": "One name",
+                "name": "One nameA",
                 "Level": "One",
                 "Priority": "Highest",
+
                 "SubdataA": {
                             "name": "One nameSubdataA",
                             "Level": "One",
                             "Priority": "Highest",
                             },
+
                 "SubdataA2": {
                             "name": "One nameSubdataA2",
                             "Level": "Two",
@@ -17,7 +19,7 @@ dictionary = {
                                         "Level": "One",
                                         "Priority": "Highest",
                                         }
-                            }
+                             }
     },
 
     "DataB": {
@@ -38,6 +40,17 @@ dictionary_of_levels = {
         "Nine": 9, "Ten": 10, "Eleven": 11, "Twelve": 12, "Thirteen": 13, "Fourteen": 14, "Fifteen": 15,
         "Sixteen": 16, "Seventeen": 17, "Eighteen": 18, "Nineteen": 19,
       }
+new_dict = {}
+for key, value in dictionary_of_levels.items():
+    new_dict[value] = key
+
+print(new_dict)
+
+
+dictionary_of_levels2 = {
+        0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', 11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 
+        14: 'Fourteen', 15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'
+                        }
 
 dictionary_of_priority = {
         "Lowest": 0, "Low": 1, "Medium": 2, "High": 3, "Highest": 4,
